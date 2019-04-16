05ade3974f424b9c9d913d5efe6d158e7fc094bb
Pierre Boyeau <pierre.boyeau@gmail.com>
2019-04-16 16:00:51 -0700

    refactor metrics and model eval, delete first attempt

--- ppc/metrics.py
+++ ppc/metrics.py
@@ -4,27 +4,31 @@ import numpy as np
 
 
 class Metric:
-    def __init__(self, trainer, phi_name=None):
+    def __init__(self, trainer, phi_name=None, n_sample_posterior=25, batch_size=32):
+        phi_tag = '_'+phi_name if phi_name is not None else ''
+        self.name = "abstract_metric{}".format(phi_tag)
         self.trainer = trainer
         self.phi = None
-
+        self.n_sample_posterior = n_sample_posterior
+        self.batch_size = batch_size
         self.init_phi(phi_name=phi_name)
 
     def compute(self):
         pass
 
-    def save_csv(self):
+    def _csv(self):
         pass
 
     def plot(self):
         pass
 
-    def generate(self, n_sample_posterior=25, batch_size=32):
+    def generate(self):
+        is_zero_inflated = self.trainer.model.reconstruction_loss == 'zinb'
         x_gen, x_real = self.trainer.train_set.generate(genes=None,
-                                                        n_samples=n_sample_posterior,
-                                                        zero_inflated=False,
-                                                        batch_size=batch_size)
-        return x_gen, x_real
+                                                        n_samples=self.n_sample_posterior,
+                                                        zero_inflated=is_zero_inflated,
+                                                        batch_size=self.batch_size)
+        return x_gen.squeeze(), x_real
 
     @staticmethod
     def phi_ratio(array, axis=None):
@@ -55,50 +59,60 @@ class Metric:
         elif phi_name == 'cv':
             self.phi = self.phi_cv
 
+    def reset_trainer(self, new_trainer):
+        self.trainer = new_trainer
+
 
 class LikelihoodMetric(Metric):
-    def __init__(self, trainer):
-        super().__init__(trainer=trainer)
+    def __init__(self, verbose=False, n_mc_samples=1000, **kwargs):
+        super().__init__(**kwargs)
+        self.name = "likelihood"
+        self.verbose = verbose
+        self.n_mc_samples = n_mc_samples
 
-    def compute(self, verbose=False, n_mc_samples=1000):
-        ll = self.trainer.test_set.marginal_ll(verbose=verbose,
-                                               n_mc_samples=n_mc_samples)
-        return ll
+    def compute(self):
+        ll = self.trainer.test_set.marginal_ll(verbose=self.verbose,
+                                               n_mc_samples=self.n_mc_samples)
+        return {'ll': ll}
 
 
 class ImputationMetric(Metric):
-    def __init__(self, trainer):
-        super().__init__(trainer=trainer)
+    def __init__(self, n_samples_imputation=1, **kwargs):
+        super().__init__(**kwargs)
+        self.name = "imputation_metric"
+        self.n_samples_imputation = n_samples_imputation
         
-    def compute(self, n_samples_imputation=1):
-        
-        original_list, imputed_list = self.trainer.train_set.imputation_benchmark(verbose=True, n_samples=n_samples_imputation, show_plot=False)
-        
+    def compute(self):
+        original_list, imputed_list = self.trainer.train_set.imputation_benchmark(verbose=True,
+                                                                                  n_samples=self.n_samples_imputation,
+                                                                                  show_plot=False)
         imputation_errors = np.abs(np.concatenate(original_list) - np.concatenate(imputed_list))
         median_imputation_score = np.median(imputation_errors)
         
-        return median_imputation_score
+        return {'median_imputation_score': median_imputation_score}
 
 
 class DifferentialExpressionMetric(Metric):
-    def __init__(self, trainer):
-        super().__init__(trainer=trainer)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.name = "differential_expression_metric"
 
 
 class SummaryStatsMetric(Metric):
-    def __init__(self, trainer, phi_name, stat_name='tstat'):
-        super().__init__(trainer=trainer, phi_name=phi_name)
+    def __init__(self, stat_name='tstat', **kwargs):
+        super().__init__(**kwargs)
+        assert self.phi is not None
+        self.name = "summary_stats_metric"
         self.stat = None
-
         self.init_stat(stat_name)
 
-    def compute(self, n_sample_posterior=25, batch_size=32):
-        x_gen, x_real = self.generate(n_sample_posterior, batch_size)
+    def compute(self):
+        x_gen, x_real = self.generate()
         phi_real_gene = self.phi(x_real, axis=0)
         phi_gen_gene = self.phi(x_gen, axis=0)
 
         stat_phi, pvals = self.stat(phi_real_gene, phi_gen_gene, axis=0)
-        return stat_phi, pvals
+        return {"stat_phi": stat_phi, "pvals": pvals}
 
     def init_stat(self, stat_name):
         if stat_name == 'tstat':
