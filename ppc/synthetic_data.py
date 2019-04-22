0a30a8ed64610b66b09c6a2fdb9717817529c38f
Oscar Clivio <oclivio@live.fr>
2019-04-22 09:41:40 -0700

    Synthetic data changed

--- ppc/synthetic_data.py
+++ ppc/synthetic_data.py
@@ -3,8 +3,8 @@ from scvi.dataset import GeneExpressionDataset
 
 
 class SyntheticDataset(GeneExpressionDataset):
-    def __init__(self, batch_size=200, nb_genes=100, n_batches=2, n_labels=3, mode='zi',
-                 nb_genes_zi=None):
+    def __init__(self, batch_size=5000, nb_genes=1200, n_batches=2, n_labels=3, mode='zi',
+                 ratio_genes_zi=None):
         np.random.seed(0)
         # Generating samples according to a ZINB process
         data = np.random.negative_binomial(5, 0.3, size=(n_batches, batch_size, nb_genes))
@@ -15,8 +15,9 @@ class SyntheticDataset(GeneExpressionDataset):
         elif mode == 'nb':
             newdata = data
         else:
-            assert nb_genes_zi is not None
-            assert nb_genes_zi <= nb_genes
+            assert ratio_genes_zi is not None
+            assert ratio_genes_zi <= 1.
+            nb_genes_zi = int(nb_genes * ratio_genes_zi)
             submask = np.random.binomial(n=1, p=0.7, size=(n_batches, batch_size, nb_genes_zi))
             mask = np.ones_like(data)
             mask[:, :, :nb_genes_zi] = submask
@@ -40,14 +41,14 @@ class ZINBDataset(SyntheticDataset):
 
 class Mixed25Dataset(SyntheticDataset):
     def __init__(self):
-        super().__init__(mode='mixed', nb_genes=100, nb_genes_zi=25)
+        super().__init__(mode='mixed', ratio_genes_zi=.25)
 
 
 class Mixed50Dataset(SyntheticDataset):
     def __init__(self):
-        super().__init__(mode='mixed', nb_genes=100, nb_genes_zi=50)
+        super().__init__(mode='mixed', ratio_genes_zi=.5)
 
 
 class Mixed75Dataset(SyntheticDataset):
     def __init__(self):
-        super().__init__(mode='mixed', nb_genes=100, nb_genes_zi=75)
+        super().__init__(mode='mixed', ratio_genes_zi=.75)
