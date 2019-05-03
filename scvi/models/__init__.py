9a0c8d3d5dfd737f7e753df9cf3e6c63d7106dc2
adamgayoso <adamgayoso@users.noreply.github.com>
2019-05-02 21:57:23 -0700

    final fix to init

--- scvi/models/__init__.py
+++ scvi/models/__init__.py
@@ -3,6 +3,7 @@ from .scanvi import SCANVI
 from .vae import VAE, LDVAE
 from .vae_fish import VAEF
 from .vaec import VAEC
+from .vae_atac import VAE_ATAC
 
 __all__ = ['SCANVI',
            'VAEC',
